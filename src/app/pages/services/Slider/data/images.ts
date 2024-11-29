8b7f0b9baa7792a1a1a272e37116db8f3dbb1a5d
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2024-11-29 17:34:29 +0100

    refactor : rename services images

--- src/app/pages/services/Slider/data/images.ts
+++ src/app/pages/services/Slider/data/images.ts
@@ -1,7 +1,19 @@
-import image1 from "@/app/assets/services/1-2x.jpg";
-import image2 from "@/app/assets/services/2-2x.jpg";
-import image3 from "@/app/assets/services/3-2x.jpg";
-import image4 from "@/app/assets/services/4-2x.jpg";
-import image5 from "@/app/assets/services/5-2x.jpg";
+import image1_1x from "@/app/assets/services/ATVS-1x.jpg";
+import image3_1x from "@/app/assets/services/hotAirBallooning-1x.jpg";
+import image5_1x from "@/app/assets/services/rafting-1x.jpg";
+import image2_1x from "@/app/assets/services/rockClimbing-1x.jpg";
+import image4_1x from "@/app/assets/services/skyDiving-1x.jpg";
 
-export const images = [image1, image2, image3, image4, image5];
+import image1_2x from "@/app/assets/services/ATVS-2x.jpg";
+import image3_2x from "@/app/assets/services/hotAirBallooning-2x.jpg";
+import image5_2x from "@/app/assets/services/rafting-2x.jpg";
+import image2_2x from "@/app/assets/services/rockClimbing-2x.jpg";
+import image4_2x from "@/app/assets/services/skyDiving-2x.jpg";
+
+export const images = [
+  image1_2x || image1_1x,
+  image2_2x || image2_1x,
+  image3_2x || image3_1x,
+  image4_2x || image4_1x,
+  image5_2x || image5_1x,
+];
